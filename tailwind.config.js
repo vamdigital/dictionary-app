854ce1f51ba4d393ee409dea6223a37c6ead95e6
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2023-05-17 18:00:30 +0100

    fix((font change)): font change not working
    Font change not reflecting via css dynamic class added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './app/**/*.{js,jsx,ts,tsx}'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
